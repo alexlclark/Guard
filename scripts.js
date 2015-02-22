281a3ae224ac497b0e44dbd05d0256f43097c79b
alexlclark <alexclarkcontact@gmail.com>
2015-02-21 21:03:40 -0500

    Edited height of chart and title.

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
- google.load('visualization', '1.0', {'packages':['corechart']});
+google.load('visualization', '1.0', {'packages':['corechart']});
 
-      // Set a callback to run when the Google Visualization API is loaded.
-      google.setOnLoadCallback(drawChart);
+// Set a callback to run when the Google Visualization API is loaded.
+google.setOnLoadCallback(drawChart);
 
-      // Callback that creates and populates a data table,
-      // instantiates the pie chart, passes in the data and
-      // draws it.
-      function drawChart() {
-      	
-      	//NEW - Use a for loop to  make an array of arrays out of relevant JSON data
-      	var arraysFat = [] //created big empty array to hold smaller date/val arrays0
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
 
-      	for (var i=0;/*spacesdont  matter in for loops*/i<jsonFREDData.observations.length; i++){ //all for loops ever will appear like this. start, length, 
+//NEW - Use a for loop to  make an array of arrays out of relevant JSON data
+var arraysFat = [] //created big empty array to hold smaller date/val arrays0
+
+for (var i=0;/*spacesdont  matter in for loops*/i<jsonFREDData.observations.length; i++){ //all for loops ever will appear like this. start, length,
 
 var itemArray=[];
 itemArray.push(jsonFREDData.observations[i].date);//adding date and value to item array (the small array)
@@ -22,21 +22,20 @@ arraysFat.push(itemArray); //add my little array to the larger one, arraysFat
 
 
 }
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn('string', 'date');
-        data.addColumn('number', 'value');
-        data.addRows(arraysFat); // this has to equal the array at the top (the fat one)
+// Create the data table.
+var data = new google.visualization.DataTable();
+data.addColumn('string', 'date');
+data.addColumn('number', 'value');
+data.addRows(arraysFat); // this has to equal the array at the top (the fat one)
 
-        // Set chart options
-        var options = {'title':'Dates and Values from FRED db',
-                       'width':800,
-                       'height':1800};
+// Set chart options
+var options = {'title':'Litres of Vodka Exported by Year',
+'width':800,
+'height':1700};
 
-        // Instantiate and draw our chart, passing in some options.
-        var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
-      
-      
-     
+// Instantiate and draw our chart, passing in some options.
+var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+chart.draw(data, options);
+}
+
+
