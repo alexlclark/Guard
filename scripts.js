c45703963a866b0085385ccb8bca04d2f7e17ac1
alexlclark <alexclarkcontact@gmail.com>
2015-04-12 18:47:31 -0400

    updated scripts rowdata and corrected if statement with expanded fields

--- scripts.js
+++ scripts.js
@@ -1,34 +1,30 @@
-*/for (var i=0; i<guardianHeroinData.length; i++) {
+for (var i=0; i<guardianHeroinData.length; i++) {
+	var rowData = guardianHeroinData[i];
+	
+	var $template = $('<div class="quoteContainer">'+
+						'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
+						'<div class="readMore">Read More</div>'+
+						'<div class="moreInfo">'+
+							'<div class="source">'+rowData.source+'</div>'+
+							'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
+							'<div class="location">'+rowData.location+'</div>'+
+							'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
+						'</div>'+
+					'</div>');
 
-var rowData = guardianHeroinData[i];
+$template.find(".readMore").on("click", toggleContent)
 
-var $template = $('<div class="quoteContainer" id=quote-'+i+>'+
-'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
-'<div class="readMore">Read More</div>'+
-'<div class="moreInfo">'+
-'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
-'<div class="yearsClean">'+rowData.source+'</div>'+
-'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
-'<div class="Location">'+rowData.location+'</div>'+
-'</div>'+
-'</div>');
-
-$(template).find("readMore").on("click", toggleContent);
-
-function toggleContent(e) {
+function toggleContent(){
 	
 	$quoteContainer = $(this).parent();
 	
-	if ($quoteContainer.hasClass("expanded")) { // quoteContainer has class .expanded (you can see content!, so remove content
-		$quoteContainer.removeClass("expanded");
-		
-	}else { //quoteContainer does not have class expanded (you can't see content), so show content!
-	
-	$quoteContainer.addClass("expanded");
-	
-}
+	if($quoteContainer.hasClass("expanded")){// quoteContainer has class .expanded (you can see content!), so remove content!
+	   $quoteContainer.removeClass("expanded");
+	}else{//quoteContainer does not have calsss expanded (you can't see content), so show content!
+	   $quoteContainer.addClass("expanded");
+	}
 }
 
-$('#dataContainer').append(template);
-}/*
+$('#dataContainer').append($template);
 	
+}
