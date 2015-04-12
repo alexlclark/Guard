b76a242fa9873a2f5a338e0251c9c398720db037
alexlclark <alexclarkcontact@gmail.com>
2015-04-12 19:10:52 -0400

    updated mainQuotes to artNEWS headers

--- data.js
+++ data.js
@@ -1,30 +1,23 @@
 var artNewsData = [  
     {  
-        "mainQuote":"For the past 26 years I\'ve thought of heroin every day",
+        "mainQuote":"Secret Grandeur: Leo Steinberg on an Early Velazquez Masterpiece, in 1971",
         "wholeQuote":"But never thought of using it. It never made anything better, just worse. Look, every junky is always â€œconsideringâ€ getting clean. Every junky wants to be clean. Every using junky also wants more heroin. Call it a contradiction call it what you want. But no junky wants the life they have.",
         "source":"James",
         "yearsClean":"26 years clean",
         "location":"Sydney, Australia"
     },
     {  
-        "mainQuote":"When I first gave up heroin, I could never tell myself it was forever",
+        "mainQuote":"Shrimp, Female Bacteria, and the Smell of Gagosian: Anicka Yi",
         "wholeQuote":"I woke up last week to the radio playing quietly. I got ready for the trek to the other end of the house to wake up the kid. The 7:30am news came on. I was still sleepy. â€œThe actor Phillip Seymour Hoffman is dead.â€ I was only half listening. I'd seen his work but have never been one to pay much attention to famous personalities or celebrities. â€œHe was forty-six.â€ That caught half my attention â€“ the same age as me. â€œFrom a heroin overdose.â€ There I was, sitting on the edge of my bed with a syringe full of heroin in my hand. All at once I felt a kindred with someone I had never known. I knew a couple of characters he'd played, I knew his name, but that was it. Now suddenly, he was a lot like me.When you're addicted to heroin, It\'s generally a long-running, close relationship.  There are plenty of bad times, but there are also gems that stay with you. It\'s like family â€“ a parent, sibling or child. You may swear that for all they've put you through, you never want anything to do with them again, but there's always that part of you that misses the good times and wonders how you'd get along now. When I first gave up heroin, I could never tell myself it was forever. I could not imagine never entering that cosy cocoon again.",
         "source":"Linda",
         "yearsClean":"still using",
         "location":"Geelong, Australia"
     },
     {  
-        "mainQuote":"I see what happened to Philip Seymour Hoffman everyday in my hometown",
+        "mainQuote":"Ideas Worth Spreading: Simon Denny Comes to MoMA PS1",
         "wholeQuote":"My father died from addiction, we lost a cousin, I myself had numerous overdoses and helped prevent siblings from OD-ing (all of who are now clean thanks to the process of one addict helping another that Russell Brand spoke of). Hearing about Hoffman affects me the same way as if it were a friend. I didnt know he was an addict untill he died, but when I heard of his story and addiction I immediatly identified with him. We work the same way in our minds. I know what happened to him before he picked up. I know how he felt. I'm pretty sure it started the same way most relapses do. ",
         "source":"Danny",
         "yearsClean":"10 years clean",
         "location":"Dublin, Ireland"
-    },
-    {  
-        "mainQuote":"I live in real fear that I'll relapse",
-        "wholeQuote":"This is what scares me late at night: If somebody like Hoffman had gone so long without smack (heroin), and just jumped back in one day â€“ how close am I to doing the same? I live in real fear that I'll relapse, and being so much older now, there's no way I'd survive this time around.",
-        "source":"Gene",
-        "yearsClean":"38 years clean",
-        "location":"San Diego, California"
     }
 ]
